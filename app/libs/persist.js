1441b96859cc1108f1072c9969b87bb16cfae413
Dario Carella <dario.kar@gmail.com>
2016-03-17 00:10:56 +0100

    Removed useless logging

--- app/libs/persist.js
+++ app/libs/persist.js
@@ -4,8 +4,6 @@ export default function(alt, storage, storeName) {
   const finalStore = makeFinalStore(alt);
 
   try {
-    console.log(storage.get(storeName));
-
     alt.bootstrap(storage.get(storeName));
   } catch (e) {
     console.error('Failed to bootstrap data', e);

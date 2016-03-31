8e1add3b652de9f3d1cbaf423ef70f5c516ef924
Dario Carella <dario.kar@gmail.com>
2016-03-31 19:45:59 +0200

    Added empty button in prompt component

--- app/components/Prompt.jsx
+++ app/components/Prompt.jsx
@@ -7,41 +7,49 @@ export default class Prompt extends React.Component {
   };
 
   render = () => {
-    let { icon, color, size, fixed, position, disabled, placeholder } = this.props;
+    let { icon, color, size, fixed, position, disabled, placeholder, empty } = this.props;
     const { text } = this.state;
 
     if (!icon)
       icon = this.defaultIcon;
 
-    let promptClasses = classnames('prompt', {
-      'prompt--primary': color === 'primary',
-      'prompt--success': color === 'success',
-      'prompt--info': color === 'info',
-      'prompt--warning': color === 'warning',
-      'prompt--error': color === 'error',
-      'prompt--small': size === 'small',
-      'prompt--medium': size === 'medium',
-      'prompt--big': size === 'big',
-      'prompt--fixed': fixed,
-      'prompt--top-left': position === 'top-left',
-      'prompt--top-right': position === 'top-right',
-      'prompt--bottom-left': position === 'bottom-left',
-      'prompt--bottom-right': position === 'bottom-right',
-      'prompt--disabled': disabled === true
-    });
+    const styles = {
+      prompt: classnames('prompt', {
+        'prompt--primary'     : color === 'primary',
+        'prompt--success'     : color === 'success',
+        'prompt--info'        : color === 'info',
+        'prompt--warning'     : color === 'warning',
+        'prompt--error'       : color === 'error',
+        'prompt--small'       : size === 'small',
+        'prompt--medium'      : size === 'medium',
+        'prompt--big'         : size === 'big',
+        'prompt--fixed'       : fixed,
+        'prompt--top-left'    : position === 'top-left',
+        'prompt--top-right'   : position === 'top-right',
+        'prompt--bottom-left' : position === 'bottom-left',
+        'prompt--bottom-right': position === 'bottom-right',
+        'prompt--disabled'    : disabled === true
+      }),
+      icon: classnames('fa', `fa-${icon}`),
+      empty: classnames('fa', 'fa-times')
+    };
 
-    let iconClasses = classnames('fa', `fa-${icon}`);
+    const faLink = (icon || empty) ? (<link
+      rel={'stylesheet'} href={'//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'} />) : null;
+    const iconElement = (icon) ? (<i className={styles.icon}></i>) : null;
+    const emptyElement = (icon) ? (<i className={styles.empty} onClick={this.handleEmpty}></i>) : null;
 
     return (
       <div
-        className={promptClasses}>
-        <link rel={'stylesheet'} href={'//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'} />
-        <i className={iconClasses}></i>
+        className={styles.prompt}>
+        {faLink}
+        {iconElement}
         <input
           type={'text'}
           placeholder={placeholder}
           value={text}
           onChange={this.handleChange} />
+        {emptyElement}
       </div>
     );
   };
@@ -54,5 +62,16 @@ export default class Prompt extends React.Component {
     this.setState({text: newText});
 
     onChange({ oldText, newText });
+  };
+
+  handleEmpty = () => {
+    const oldText = this.state.text;
+    const newText = '';
+    const { onChange } = this.props;
+
+    this.setState({ text: newText });
+
+    onChange({ oldText, newText });
+
   };
 }

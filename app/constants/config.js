e8fbae7728ca33c06bb3b8e610986773b0d4979e
Dario Carella <dario.kar@gmail.com>
2016-04-02 19:02:32 +0200

    jscs config fixes

--- app/constants/config.js
+++ app/constants/config.js
@@ -33,67 +33,81 @@ export default {
         {
           'featureType': 'administrative.country',
           'elementType': 'geometry.stroke',
-          'stylers'    : [{'lightness': '40'}, {'visibility': 'on'}]
+          'stylers': [{'lightness': '40'}, {'visibility': 'on'}]
         },
         {
           'featureType': 'administrative.country',
           'elementType': 'labels',
-          'stylers'    : [{'visibility': 'simplified'}, {'lightness': '20'}]
+          'stylers': [{'visibility': 'simplified'}, {'lightness': '20'}]
         },
         {
           'featureType': 'administrative.province',
           'elementType': 'geometry.stroke',
-          'stylers'    : [{'visibility': 'off'}]
+          'stylers': [{'visibility': 'off'}]
         },
         {
           'featureType': 'administrative.province',
           'elementType': 'labels',
-          'stylers'    : [{'visibility': 'simplified'}, {'lightness': '10'}]
+          'stylers': [{'visibility': 'simplified'}, {'lightness': '10'}]
         },
         {
           'featureType': 'administrative.locality',
           'elementType': 'geometry.stroke',
-          'stylers'    : [{'visibility': 'on'}]
+          'stylers': [{'visibility': 'on'}]
         },
         {
           'featureType': 'administrative.locality',
           'elementType': 'labels',
-          'stylers'    : [{'lightness': '25'}]
+          'stylers': [{'lightness': '25'}]
         },
         {
           'featureType': 'landscape',
           'elementType': 'all',
-          'stylers'    : [{'hue': '#ffbb00'}, {'saturation': 43.400000000000006}, {'lightness': 37.599999999999994}, {'gamma': 1}]
+          'stylers': [
+            {'hue': '#ffbb00'},
+            {'saturation': 43.400000000000006},
+            {'lightness': 37.599999999999994}, {'gamma': 1}
+          ]
         },
         {
           'featureType': 'poi',
           'elementType': 'all',
-          'stylers'    : [{'hue': '#00ff6a'}, {'saturation': -1.0989010989011234}, {'lightness': 11.200000000000017}, {'gamma': 1}]
+          'stylers': [
+            {'hue': '#00ff6a'},
+            {'saturation': -1.0989010989011234},
+            {'lightness': 11.200000000000017},
+            {'gamma': 1}
+          ]
         },
         {
           'featureType': 'road',
           'elementType': 'all',
-          'stylers'    : [{'visibility': 'on'}, {'lightness': '30'}]
+          'stylers': [{'visibility': 'on'}, {'lightness': '30'}]
         },
         {
           'featureType': 'road.highway',
           'elementType': 'all',
-          'stylers'    : [{'hue': '#ffc200'}, {'saturation': -61.8}, {'lightness': 45.599999999999994}, {'gamma': 1}]
+          'stylers': [{'hue': '#ffc200'}, {'saturation': -61.8}, {'lightness': 45.599999999999994}, {'gamma': 1}]
         },
         {
           'featureType': 'road.arterial',
           'elementType': 'all',
-          'stylers'    : [{'hue': '#ff0300'}, {'saturation': -100}, {'lightness': 51.19999999999999}, {'gamma': 1}]
+          'stylers': [{'hue': '#ff0300'}, {'saturation': -100}, {'lightness': 51.19999999999999}, {'gamma': 1}]
         },
         {
           'featureType': 'road.local',
           'elementType': 'all',
-          'stylers'    : [{'hue': '#ff0300'}, {'saturation': -100}, {'lightness': 52}, {'gamma': 1}]
+          'stylers': [{'hue': '#ff0300'}, {'saturation': -100}, {'lightness': 52}, {'gamma': 1}]
         },
         {
           'featureType': 'water',
           'elementType': 'all',
-          'stylers'    : [{'hue': '#0078FF'}, {'saturation': -13.200000000000003}, {'lightness': 2.4000000000000057}, {'gamma': 1}]
+          'stylers': [
+            {'hue': '#0078FF'},
+            {'saturation': -13.200000000000003},
+            {'lightness': 2.4000000000000057},
+            {'gamma': 1}
+          ]
         },
         {
           'featureType': 'water',
